0c366962d36b5b0c0bfee01b6326ad36b726a482
HarukeyUA <inazarrusnak@gmail.com>
2019-06-27 07:14:28 +0000

    Revert "Revert "msm8937-common: Add basic USB HAL that reports no status change""
    This reverts commit aece0f27ffcf16f21d8c97214ca225b68583adc3.

--- msm8937.mk
+++ msm8937.mk
@@ -366,7 +366,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.xiaomi_8937
 
 # Vibrator
 PRODUCT_PACKAGES += \
